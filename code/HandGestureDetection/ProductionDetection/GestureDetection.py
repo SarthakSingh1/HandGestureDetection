d20faf54f9ed399c18080455f8a382d25fade651
sarthaksingh94 <singh94@illinois.edu>
2024-03-31 23:22:32 -0500

    gpio variable updated properly

--- code/HandGestureDetection/ProductionDetection/GestureDetection.py
+++ code/HandGestureDetection/ProductionDetection/GestureDetection.py
@@ -9,6 +9,34 @@ tips = [8, 12, 16, 20]  # 20 landmarks total and these are all the tip of each f
 joints = [6, 10, 14, 18]
 fingers_name = ["Index", "Middle", "Ring", "Pinky"]
 
+gestures = {
+    "rock_roll": [1, 0, 0, 1],
+    "peace": [1, 1, 0, 0],
+    "index_up": [1, 0, 0, 0],
+    "all_fingers_up": [1, 1, 1, 1],
+    "pinky": [0, 0, 0, 1],
+    "closed_hand": [0, 0, 0, 0],
+}
+
+gestures_id = {
+    "rock_roll": 1,
+    "peace": 2,
+    "index_up": 3,
+    "all_fingers_up": 4,
+    "pinky": 5,
+    "closed_hand": 0,
+}
+
+
+def match_gesture(fingers):
+    for gesture, pattern in gestures.items():
+        if fingers == pattern:
+            return gesture, gestures_id[gesture]
+    return "unknown", -1
+
+
+curr_state = "Nothing"
+gpio_value = 0
 
 while True:
     ret, frame = cameraCapture.read()  # Gets a frame
@@ -36,7 +64,17 @@ while True:
             else:
                 finger = "down"
                 down += 1
-            print(f"{fingers_name[i]} is {finger}")
-        print(f"Total Fingers Up: {up}  Down: {down}")
+        #     print(f"{fingers_name[i]} is {finger}")
+        # print(f"Total Fingers Up: {up}  Down: {down}")
 
-    time.sleep(1)
+        gpio_temp, state = match_gesture(fingers)
+
+        if gpio_temp == -1:
+            continue
+        else:
+            gpio_value = gpio_temp
+            curr_state = state
+            print(f"New gesture detected {curr_state}")
+            
+    print(f"Current state: {curr_state} current gpio: {gpio_value}")
+
