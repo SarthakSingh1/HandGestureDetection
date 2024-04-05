6fc5a14a9254f79646bb6a7319f5ef0d49855988
zbaum2 <zbaum2@illinois.edu>
2024-04-05 18:46:34 -0500

    GPIO from camera stuff coded, no syntax errors but unable to test audio

--- code/audio_effects/main.c
+++ code/audio_effects/main.c
@@ -24,7 +24,7 @@
 
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c2;
+
 
 I2S_HandleTypeDef hi2s2;
 DMA_HandleTypeDef hdma_spi2_tx;
@@ -38,6 +38,7 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_I2C2_Init(void);
 static void MX_I2S2_Init(void);
+
 
 
 /*
@@ -101,6 +102,7 @@ uint8_t callback_state = 0;
 uint8_t useReverb = 0; // Control flag for reverb effect
 uint8_t useLPF = 0; // Control flag for low-pass filter effect
 uint8_t useHPF = 0; // Control flag for high-pass filter effect
+uint8_t useDistortion = 0;
 
 float gainFactorDown = 0.8; // Example gain factor for decreasing volume
 float gainFactorUp = 1.2; // Example gain factor for increasing volume
@@ -177,12 +179,13 @@ int main(void)
   MX_I2C2_Init();
   MX_I2S2_Init();
   MX_USB_DEVICE_Init();
+  MX_I2C2_Init();
 
 
   HAL_I2SEx_TransmitReceive_DMA (&hi2s2, txBuf, rxBuf, 4);
 
 
-  while (1) 
+  while (1)
   {
       ReadGesture(); // Call this at regular intervals
           // Other tasks...
@@ -195,7 +198,7 @@ int main(void)
 void Process_LPF(float* input, float* output, int blockSize) {
     for (int n = 0; n < blockSize; ++n) {
         float outSample = 0.0f;
-        for (int i = 0; i < LPF_TAP_NUM; ++i) {
+        for (int i = 0; i < FILTER_TAP_NUM; ++i) {
             int index = (n - i + blockSize) % blockSize; // Circular buffer indexing
             outSample += input[index] * lpf_taps[i];
         }
@@ -207,7 +210,7 @@ void Process_LPF(float* input, float* output, int blockSize) {
 void Process_HPF(float* input, float* output, int blockSize) {
     for (int n = 0; n < blockSize; ++n) {
         float outSample = 0.0f;
-        for (int i = 0; i < HPF_TAP_NUM; ++i) {
+        for (int i = 0; i < FILTER_TAP_NUM; ++i) {
             int index = (n - i + blockSize) % blockSize; // Circular buffer indexing
             outSample += input[index] * hpf_taps[i];
         }
@@ -221,12 +224,12 @@ void Process_HPF(float* input, float* output, int blockSize) {
   */
 
 void HAL_I2SEx_TxRxHalfCpltCallback(I2S_HandleTypeDef *hi2s) {
-    callbackState = 1; // Indicate half transfer complete
+    callback_state = 1; // Indicate half transfer complete
     ProcessAudioEffects(); // Function to decide and call the appropriate effect
 }
 
 void HAL_I2SEx_TxRxCpltCallback(I2S_HandleTypeDef *hi2s) {
-    callbackState = 2; // Indicate full transfer complete
+    callback_state = 2; // Indicate full transfer complete
     ProcessAudioEffects(); // Function to decide and call the appropriate effect
 }
 
@@ -387,34 +390,8 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_I2C2_Init(void)
-{
 
-  /* USER CODE BEGIN I2C2_Init 0 */
 
-  /* USER CODE END I2C2_Init 0 */
-
-  /* USER CODE BEGIN I2C2_Init 1 */
-
-  /* USER CODE END I2C2_Init 1 */
-  hi2c2.Instance = I2C2;
-  hi2c2.Init.ClockSpeed = 100000;
-  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c2.Init.OwnAddress1 = 0;
-  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c2.Init.OwnAddress2 = 0;
-  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C2_Init 2 */
-
-  /* USER CODE END I2C2_Init 2 */
-
-}
 
 /**
   * @brief I2S2 Initialization Function
@@ -483,16 +460,16 @@ static void MX_GPIO_Init(void)
     __HAL_RCC_GPIOC_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE(); // Ensure GPIO port A's clock is enabled
-
-    /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-
-    /*Configure GPIO pin : LED_Pin */
-    GPIO_InitStruct.Pin = LED_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+//
+//    /*Configure GPIO pin Output Level */
+//    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+//
+//    /*Configure GPIO pin : LED_Pin */
+//    GPIO_InitStruct.Pin = LED_Pin;
+//    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+//    GPIO_InitStruct.Pull = GPIO_NOPULL;
+//    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//    HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
     // Additional code for configuring GPIOA pins as input
     __HAL_RCC_GPIOA_CLK_ENABLE();
